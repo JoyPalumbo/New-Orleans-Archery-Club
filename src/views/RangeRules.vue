3b578674e304d872667b1aa8b9fa1f03a0b5c82a
Joy <greeniejoy@gmail.com>
2020-11-08 20:21:35 -0600

    added content to range rules

--- src/views/RangeRules.vue
+++ src/views/RangeRules.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Range Rules</h1>
+    <h2>
+      We want to make sure every one can enjoy Archery in a fun, safe
+      environment so it is important that everyone observe these rules.
+    </h2>
+    <v-row>
+      <v-col md="1"></v-col>
+      <v-col md="11">
+        <h3 class="text-left ma-2">
+          1. Crossbows, broadheads, guns, firearms, pets and alcoholic beverages
+          are not allowed at the range.
+        </h3>
+
+        <h3 class="text-left ma-2">
+          2. Any device that locks a bow at full or partial draw is not allowed
+          at the range.
+        </h3>
+        <h3 class="text-left ma-2">
+          3. Children under the age of twelve require adult supervision.
+        </h3>
+        <h3 class="text-left ma-2">
+          4. No sky pulling. Sky pulling is holding the bow high and pulling
+          back on the arrow while lowering the bow to a shooting position. If
+          the arrow was accidently released during a sky pull the arrow could
+          endanger other people.
+        </h3>
+        <h3 class="text-left ma-2">
+          5. Members must sign a release, waiver and indemnity agreement.
+        </h3>
+        <h3 class="text-left ma-2">
+          6. No backtracking on the field range. On the field range, take your
+          bow and gear with you when you advance to pull your arrows. Do not
+          walk back up the shooting lane.
+        </h3>
+        <h3 class="text-left ma-2">
+          7. If searching for a lost arrow, place your bow in front of the
+          target. Do not commence firing if you observe a bow in front of the
+          target.
+        </h3>
+      </v-col>
+    </v-row>
   </div>
 </template>
