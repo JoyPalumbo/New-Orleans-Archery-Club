488f7bf4287dbaeca87185be4956b8090bc17209
Joy <greeniejoy@gmail.com>
2020-11-08 23:58:42 -0600

    deleted vue logo favicon

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <h1 font-size="25px">Meet the Coaches and Instructors</h1>
+      <!-- <v-col> -->
+      <!-- </v-col> -->
+      <v-col> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+/* h1 {
+  font-size: 15;
+} */
+</style>
