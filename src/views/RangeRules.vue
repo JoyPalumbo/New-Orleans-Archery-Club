006f132bf9c8100d98f02b99b04315cff4c5aa11
Joy <greeniejoy@gmail.com>
2020-11-08 22:19:46 -0600

    centered text

--- src/views/RangeRules.vue
+++ src/views/RangeRules.vue
@@ -6,35 +6,35 @@
       environment so it is important that everyone observe these rules.
     </h2>
     <v-row>
-      <v-col md="1"></v-col>
+      <!-- <v-col md="1"></v-col> -->
       <v-col md="11">
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           1. Crossbows, broadheads, guns, firearms, pets and alcoholic beverages
           are not allowed at the range.
         </h3>
 
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           2. Any device that locks a bow at full or partial draw is not allowed
           at the range.
         </h3>
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           3. Children under the age of twelve require adult supervision.
         </h3>
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           4. No sky pulling. Sky pulling is holding the bow high and pulling
           back on the arrow while lowering the bow to a shooting position. If
           the arrow was accidently released during a sky pull the arrow could
           endanger other people.
         </h3>
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           5. Members must sign a release, waiver and indemnity agreement.
         </h3>
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           6. No backtracking on the field range. On the field range, take your
           bow and gear with you when you advance to pull your arrows. Do not
           walk back up the shooting lane.
         </h3>
-        <h3 class="text-left ma-2">
+        <h3 class="text-left ma-2 text-center">
           7. If searching for a lost arrow, place your bow in front of the
           target. Do not commence firing if you observe a bow in front of the
           target.
@@ -43,3 +43,9 @@
     </v-row>
   </div>
 </template>
+
+<style>
+h3 {
+  text-align: center;
+}
+</style>
