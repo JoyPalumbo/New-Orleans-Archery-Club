5b437e96574fdbb221c938ce8fb29cf91e6c1e79
Joy <greeniejoy@gmail.com>
2020-11-09 00:57:58 -0600

    added coach card templates

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,61 @@
 <template>
   <v-container>
     <v-row>
-      <h1 font-size="25px">Meet the Coaches and Instructors</h1>
-      <!-- <v-col> -->
-      <!-- </v-col> -->
-      <v-col> </v-col>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">
+              Name Here
+            </v-list-item-title>
+            <v-list-item-subtitle>Coach</v-list-item-subtitle>
+            <v-list-item-subtitle>A little bit about Name</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar size="120" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">
+              Name Here
+            </v-list-item-title>
+            <v-list-item-subtitle>Coach</v-list-item-subtitle>
+            <v-list-item-subtitle>A little bit about Name</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar size="120" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
+    </v-row>
+    <spacer class="ma-2"></spacer>
+    <v-row>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">
+              Name Here
+            </v-list-item-title>
+            <v-list-item-subtitle>Coach</v-list-item-subtitle>
+            <v-list-item-subtitle>A little bit about Name</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar size="120" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">
+              Name Here
+            </v-list-item-title>
+            <v-list-item-subtitle>Coach</v-list-item-subtitle>
+            <v-list-item-subtitle>A little bit about Name</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar size="120" color="grey"></v-list-item-avatar>
+        </v-list-item>
+      </v-card>
     </v-row>
   </v-container>
 </template>
-
-<style scoped>
-/* h1 {
-  font-size: 15;
-} */
-</style>
