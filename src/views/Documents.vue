af8f24ddfa536499c6b4340f5626cf7c26c390e8
Joy <greeniejoy@gmail.com>
2020-11-13 12:52:48 -0600

    added documents to documents view

---
+++ src/views/Documents.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <!-- <WebViewer /> -->
+
+    <!-- <iframe src="src/assets/files/Individual_Member_Ins_Benefits.pdf"></iframe> -->
+    <v-row>
+      <v-col md="3"></v-col>
+      <v-col md="2">
+        <a
+          href="http://noarcheryclub.com/documents/ARCHERYHOLDHARMLESS2015.pdf"
+          target="_blank"
+          >NOAC Release form</a
+        >
+      </v-col>
+      <v-col md="4">
+        <h4>:legal requirement in the event of an injury</h4>
+      </v-col>
+      <v-col md="3"></v-col>
+    </v-row>
+    <v-row>
+      <v-col md="3"></v-col>
+      <v-col md="2">
+        <a
+          href="http://noarcheryclub.com/documents/2012-InsuranceCoverage-8-8-12.pdf"
+          target="_blank"
+          >Club Insurance</a
+        >
+      </v-col>
+      <v-col md="4">
+        <h4>: general liability insurance as a member of USA Archery</h4>
+      </v-col>
+      <v-col md="3"></v-col>
+    </v-row>
+    <v-row>
+      <v-col md="3"></v-col>
+      <v-col md="2">
+        <a
+          href="http://noarcheryclub.com/documents/Individual_Member_Ins_Benefits.pdf"
+          target="_blank"
+          >Individual Member Insurance</a
+        >
+      </v-col>
+      <v-col md="4">
+        <h4>: medical insurance to our NOCA members through USA Archery</h4>
+      </v-col>
+      <v-col md="3"></v-col>
+    </v-row>
+
+    <!-- <pdf src="../assets/files/ARCHERYHOLDHARMLESS2015.pdf"></pdf> -->
+  </div>
+</template>
+
+<script>
+// import file from "../assets/files/ARCHERYHOLDHARMLESS2015.pdf";
+// import pdf from "vue-pdf";
+// import WebViewer from "../components/WebViewer";
+
+export default {
+  data() {
+    return {
+      // item: {
+      //   title: "Upload Template",
+      //   loc: file,
+      // },
+    };
+  },
+
+  methods: {
+    // downloadPDF() {},
+  },
+  // components: { WebViewer },
+};
+</script>
