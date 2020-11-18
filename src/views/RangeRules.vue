52dee6e662e004ee9996531f5cbcbce6bef76c1a
Joy <greeniejoy@gmail.com>
2020-11-18 12:53:56 -0600

    scoped styles

--- src/views/RangeRules.vue
+++ src/views/RangeRules.vue
@@ -1,53 +1,90 @@
 <template>
   <div class="about">
-    <h1 text-color="gray">Range Rules</h1>
+    <h1 text-color="gray" class="text-center">Range Rules</h1>
     <spacer class="ma-2"></spacer>
-    <h2 class="blue-grey--text ">
+    <h2 class="blue-grey--text pa-4 text-center">
       We want to make sure every one can enjoy Archery in a fun, safe
       environment so it is important that everyone observe these rules.
     </h2>
     <spacer class="ma-2"></spacer>
-    <v-row>
-      <!-- <v-col md="1"></v-col> -->
-      <v-col md="11">
-        <h3 class="text-left ma-2 text-center">
-          1. Crossbows, broadheads, guns, firearms, pets and alcoholic beverages
-          are not allowed at the range.
-        </h3>
+    <v-card class="mx-auto" max-width="800" outlined>
+      <v-row>
+        <!-- <v-col md="1"></v-col> -->
+        <v-col md="8">
+          <card-text class="text-left pa-2 ma-2 ">
+            <!-- <h3 class="text-left ma-2 "> -->
+            1. Crossbows, broadheads, guns, firearms, pets and alcoholic
+            beverages are not
+          </card-text>
+          <card-text class="text-left pa-2 ma-2 ">
+            allowed at the range.
+          </card-text>
+          <!-- </h3> -->
+          <br />
 
-        <h3 class="text-left ma-2 text-center">
-          2. Any device that locks a bow at full or partial draw is not allowed
-          at the range.
-        </h3>
-        <h3 class="text-left ma-2 text-center">
-          3. Children under the age of twelve require adult supervision.
-        </h3>
-        <h3 class="text-left ma-2 text-center">
-          4. No sky pulling. Sky pulling is holding the bow high and pulling
-          back on the arrow while lowering the bow to a shooting position. If
-          the arrow was accidently released during a sky pull the arrow could
-          endanger other people.
-        </h3>
-        <h3 class="text-left ma-2 text-center">
-          5. Members must sign a release, waiver and indemnity agreement.
-        </h3>
-        <h3 class="text-left ma-2 text-center">
-          6. No backtracking on the field range. On the field range, take your
-          bow and gear with you when you advance to pull your arrows. Do not
-          walk back up the shooting lane.
-        </h3>
-        <h3 class="text-left ma-2 text-center">
-          7. If searching for a lost arrow, place your bow in front of the
-          target. Do not commence firing if you observe a bow in front of the
-          target.
-        </h3>
-      </v-col>
-    </v-row>
+          <card-text class="text-left pa-2 ma-2 ">
+            <!-- <h3 class="text-left ma-2 "> -->
+            2. Any device that locks a bow at full or partial draw is not
+            allowed at the range.
+          </card-text>
+          <!-- </h3> -->
+          <!-- <h3 class="text-left ma-2 "> -->
+          <br />
+          <card-text class="text-left pa-2 ma-2 ">
+            3. Children under the age of twelve require adult supervision.
+          </card-text>
+          <!-- </h3> -->
+          <!-- <h3 class="text-left ma-2 "> -->
+          <br />
+          <card-text class="text-left pa-2 ma-2 ">
+            4. No sky pulling. Sky pulling is holding the bow high and pulling
+            back on the arrow
+          </card-text>
+          <card-text class="text-left pa-2 ma-2 ">
+            while lowering the bow to a shooting position. If the arrow was
+            accidently released
+          </card-text>
+          <card-text class="text-left pa-2 ma-2 ">
+            during a sky pull the arrow could endanger to people.
+          </card-text>
+          <!-- </h3> -->
+          <!-- <h3 class="text-left ma-2 "> -->
+          <br />
+          <card-text class="text-left pa-2 ma-2 ">
+            5. Members must sign a release, waiver and indemnity agreement.
+          </card-text>
+          <!-- </h3> -->
+          <!-- <h3 class="text-left ma-2 "> -->
+          <br />
+          <card-text class="text-left pa-2 ma-2 ">
+            6. No backtracking on the field range. On the field range, take your
+            bow and gear with
+          </card-text>
+          <card-text class="text-left pa-2 ma-2 ">
+            you when you advance to pull your arrows. Do not walk back up the
+            shooting lane.
+          </card-text>
+          <!-- </h3> -->
+          <!-- <h3 class="text-left ma-2 "> -->
+          <br />
+          <card-text class="text-left pa-2 ma-2 ">
+            7. If searching for a lost arrow, place your bow in front of the
+            target. Do not commence
+          </card-text>
+          <card-text class="text-left pa-2 ma-2 ">
+            firing if you observe a bow in front of the target.
+          </card-text>
+          <!-- </h3> -->
+        </v-col>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
-<style>
-h3 {
-  text-align: center;
+<style scoped>
+card-text {
+  font-size: 20px;
+  text-align: none;
+  /* color: #343643; */
 }
 </style>
