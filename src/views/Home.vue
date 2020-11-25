e8de5051b87ed674737d4fc2630ad7ab84011e91
Joy <greeniejoy@gmail.com>
2020-11-25 13:00:38 -0600

    changed font color and some text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,26 +15,32 @@
       <v-row>
         <!-- <v-col md="1"></v-col> -->
         <v-col class="text-center">
-        <h2>How to Join</h2>
-        <h3>The New Orleans Archery Club is open to anyone, of any age, who has an interest in learning archery!  The club has equipment available for use or you can bring your own equipment.
-            We shoot on Sundays at 12:30pm.  Please read the Range Rules and print and sign the consent form before coming your first time.
-        </h3>
+          <h2>How to Join</h2>
+          <v-spacer class="ma-4"></v-spacer>
           <h3>
+            The New Orleans Archery Club is open to anyone, of any age, who has
+            an interest in learning archery! The club has equipment available
+            for use or you can bring your own equipment. We shoot on Sundays at
+            12:30pm. Please read the Range Rules and print and sign the consent
+            form before coming your first time.
+          </h3>
+          <v-spacer class="ma-4"></v-spacer>
+          <h3 class="blue-grey--text pa-4 text-center">
             The New Orleans Archery Club (NOAC) mission is promoting competitive
             archery for enjoyment and sportsmanship. A non-profit volunteer
             organization; membership fees and donations fund our operating
             expenses.
           </h3>
           <v-spacer class="ma-4"></v-spacer>
-          <h3>
+          <h3 class="blue-grey--text pa-4 text-center">
             As a member of USA Archery we offer the Junior Olympic Archery
-            Development (JOAD) program and we all hold Level 3-NTS Coach
-            Certification.
+            Development (JOAD) program. Check out more information under the
+            JOAD section.
           </h3>
           <v-spacer class="ma-4"></v-spacer>
           <h3>
             We are located in Algiers at Federal City's archery range/baseball
-            field and shoot Sunday @ 12:30pm.
+            field and shoot Sunday at 12:30pm.
           </h3>
         </v-col>
       </v-row>
@@ -50,7 +56,7 @@ export default {
   name: "Home",
   components: {
     // HelloWorld
-  },
+  }
 };
 </script>
 
