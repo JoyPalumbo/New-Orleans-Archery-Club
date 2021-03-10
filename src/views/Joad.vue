52e277af50f8ffc0252fc2d4c690c2fa5cbfa990
Joy <greeniejoy@gmail.com>
2021-03-10 12:47:00 -0600

    added joad logo

---
+++ src/views/Joad.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col md="12" justify="center" align="center">
+      <img height="140" src="../assets/joadLogo.png" class="rounded " />
+    </v-col>
+
+    <v-col cols="1"></v-col>
+    <v-col cols="10">
+      <h1>
+        Junior Olympic Archery Development (JOAD) is a program of USA Archery
+        that helps archers to enjoy the sport recreationally or progress to the
+        excitement of competition!
+      </h1>
+      <v-row></v-row>
+      <br />
+      <h3>
+        JOAD offers barebow, recurve and compound archers the opportunity to
+        learn range safety and proper shooting technique in an environment that
+        also fosters focus, increased self-confidence, and team building skills.
+        JOAD is open to any youth archer aged 8 to 20 and is designed to grow
+        with the youth archer. Introductory JOAD classes teach the fundamentals
+        of proper shooting form; as the young archer develops, they will learn
+        more advanced techniques and earn achievement awards.
+      </h3>
+    </v-col>
+  </v-row>
+</template>
